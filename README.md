f63ef06694223bb12aa5c2e23d26d2fdbfbf142e
Greg Chapman <gregc@mac.com>
2021-05-19 13:47:45 -0700

    Add setup.py so I can (at least) "pip install -e ." and use the new humdrum converter from other projects.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # converter21
 A music21-based music notation file format converter CLI app, and some new sub converter plug-ins
 
-My goal here is to make a really accurate (and thorough) Humdrum importer and exporter for music21.
+My initial goal here is to make a really accurate (and thorough) Humdrum importer and exporter for music21.
+
+The Humdrum portion of this software is derived/translated from the C++ code in https://github.com/craigsapp/humlib, by Craig Stuart Sapp.
---
+++ setup.py
@@ -0,0 +1,30 @@
+# ------------------------------------------------------------------------------
+# Name:          setup.py
+# Purpose:       install
+#
+# Authors:       Greg Chapman
+#
+# Copyright:     (c) 2021 Greg Chapman
+# License:       BSD, see LICENSE
+# ------------------------------------------------------------------------------
+
+import setuptools
+
+converter21version = '0.9.0'
+
+if __name__ == '__main__':
+    setuptools.setup(
+        name='converter21',
+        version=converter21version,
+        author='Greg Chapman',
+        author_email='gregc@mac.com',
+        url='https://github.com/gregchapman-dev/converter21',
+        license='BSD',
+        python_requires='>=3.7',
+        description='music21-based converter tool with a new Humdrum converter',
+        long_description=open('README.md').read(),
+        packages=setuptools.find_packages(),
+        install_requires=[
+            'music21',
+        ],
+    )
