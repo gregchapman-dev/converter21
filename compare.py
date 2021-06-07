e5f66a07ad6dc9629eed47b6f65af349d0e72e88
Greg Chapman <gregc@mac.com>
2021-06-07 14:38:11 -0700

    Add timing printouts to compare.py

--- compare.py
+++ compare.py
@@ -9,6 +9,7 @@ import sys
 import os
 import resource
 import argparse
+from timeit import default_timer as timer
 from humdrum import HumdrumConverter
 
 def getInputExtensionsList() -> [str]:
@@ -70,18 +71,42 @@ sys.setrecursionlimit(1024*1024)
 #print('new stack limit =', resource.getrlimit(resource.RLIMIT_STACK))
 print('new recursion limit =', sys.getrecursionlimit())
 
+start = timer()
 score1 = m21.converter.parse(args.file1, forceSource=True)
+end = timer()
+print('parse first file took:', end - start, 'seconds')
+
+start = timer()
 score2 = m21.converter.parse(args.file2, forceSource=True)
+end = timer()
+print('parse second file took:', end - start, 'seconds')
 
 # build ScoreTrees
+start = timer()
 score_lin1 = nlin.Score(score1)
+end = timer()
+print('build first ScoreTree took:', end - start, 'seconds')
+
+start = timer()
 score_lin2 = nlin.Score(score2)
+end = timer()
+print('build second ScoreTree took:', end - start, 'seconds')
 
 # compute the complete score diff
+start = timer()
 op_list, cost = scl.complete_scorelin_diff(score_lin1, score_lin2)
+end = timer()
+print('complete_scorelin_diff took:', end - start, 'seconds')
 
 # annotate the scores to show differences
+start = timer()
 sv.annotate_differences(score1, score2, op_list)
+end = timer()
+print('annotate_differences took:', end - start, 'seconds')
 
 # display the two annotated scores
+start = timer()
 sv.show_differences(score1, score2)
+end = timer()
+print('show_differences (both scores) took:', end - start, 'seconds')
+print('')
