0f4949de6a8d5e993b7ce39da132ee068b6e9c48
Greg Chapman <75333244+gregchapman-dev@users.noreply.github.com>
2023-05-27 14:58:04 -0700

    Rename a bunch of tests.

--- tests/showdiffHumVerovioVsMeiExport.py
+++ tests/showdiffHumVerovioVsMeiExport.py
@@ -20,33 +20,6 @@ import converter21
 def runTheFullTest(krnPath: Path):
     print(f'krn file: {krnPath}')
 
-    # resultsFileName = krnPath.stem + '.json'
-    # resultsPath = krnPath.parent / resultsFileName
-
-    # import into HumdrumFile
-    hfb = HumdrumFile(str(krnPath))
-    assert hfb.isValid
-
-    # test against known good results
-    # results = HumdrumFileTestResults.fromFiles(str(krnPath), str(resultsPath))
-    # CheckHumdrumFile(hfb, results)
-
-    # import HumdrumFile into music21 stream
-    score1 = hfb.createMusic21Stream()
-    assert score1 is not None
-    assert score1.isWellFormedNotation() or not score1.elements
-
-    # export score back to humdrum (without any makeNotation fixups)
-
-    # if the score is empty, exporting from it will not produce anything interesting
-    if not score1.elements:
-        print('\tskipping export of empty score')
-        return
-
-#     score1.show('musicxml.pdf')
-#     xmlFile = score1.write('musicxml')
-#     print(f'MusicXML written to {xmlFile}')
-
     meiPath = Path(tempfile.gettempdir())
     meiPath /= krnPath.name
     meiPath = meiPath.with_suffix('.mei')
@@ -57,12 +30,12 @@ def runTheFullTest(krnPath: Path):
     )
 
     print(f'Parsing MEI file: {meiPath}')
-    score2 = m21.converter.parse(meiPath, format='mei', forceSource=True)
+    score1 = m21.converter.parse(meiPath, format='mei', forceSource=True)
 
-    assert score2 is not None
-    assert score2.isWellFormedNotation()
+    assert score1 is not None
+    assert score1.isWellFormedNotation()
 
-    meiw: MeiWriter = MeiWriter(score2)
+    meiw: MeiWriter = MeiWriter(score1)
     meiw.makeNotation = False
 
     success: bool = True
@@ -72,21 +45,19 @@ def runTheFullTest(krnPath: Path):
     with open(meiwPath, 'wb') as f:
         success = meiw.write(f)
 
-#     if not success:
-#         score1.show('musicxml.pdf')
     assert success
 
     # compare with bbdiff:
     subprocess.run(['bbdiff', str(meiPath), str(meiwPath)], check=False)
 
-    score3 = m21.converter.parse(meiwPath, format='mei', forceSource=True)
+    score2 = m21.converter.parse(meiwPath, format='mei', forceSource=True)
 
     # compare the two music21 (MEI) scores
     # with music-score-diff:
     print('comparing the two m21/MEI scores')
-    score_lin2 = AnnScore(score2, DetailLevel.AllObjectsWithStyle)
+    score_lin2 = AnnScore(score1, DetailLevel.AllObjectsWithStyle)
     print('loaded verovio MEI score')
-    score_lin3 = AnnScore(score3, DetailLevel.AllObjectsWithStyle)
+    score_lin3 = AnnScore(score2, DetailLevel.AllObjectsWithStyle)
     print('loaded my MEI score')
     diffList, _cost = Comparison.annotated_scores_diff(score_lin2, score_lin3)
     print('diffed the two scores:')
@@ -94,12 +65,12 @@ def runTheFullTest(krnPath: Path):
     print(f'\tnumber of differences = {numDiffs}')
     if numDiffs > 0:
         print('now we will mark and display the two scores')
-        Visualization.mark_diffs(score2, score3, diffList)
+        Visualization.mark_diffs(score1, score2, diffList)
         print('marked the scores to show differences')
-        Visualization.show_diffs(score2, score3)
+        Visualization.show_diffs(score1, score2)
         print('displayed both annotated scores')
-#     print('verovio MEI score written to: ', score2.write('musicxml', makeNotation=False))
-#     print('my MEI score written to: ', score3.write('musicxml', makeNotation=False))
+#     print('verovio MEI score written to: ', score1.write('musicxml', makeNotation=False))
+#     print('my MEI score written to: ', score2.write('musicxml', makeNotation=False))
     return
 
 # ------------------------------------------------------------------------------
