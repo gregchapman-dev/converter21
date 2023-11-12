17d346f92432d82cf25e182bedbc29184a5191d1
Greg Chapman <75333244+gregchapman-dev@users.noreply.github.com>
2023-11-12 13:03:32 -0800

    Humdrum export: back out the getStaffCount() fix from humlib.  It caused a few more problems than it fixed, including introducing one crash.  There's an attempt at a better fix here, commented out because it didn't help.  Some day...

--- converter21/humdrum/humgrid.py
+++ converter21/humdrum/humgrid.py
@@ -139,9 +139,26 @@ class HumGrid:
             return 0
 
         # if there is at least one measure (with at least one slice)
-        # return the number of staves in the specified part in the first slice
+        # return the number of staves in the specified part in the last slice
         # of the first measure.
-        return len(self.measures[0].slices[0].parts[partIndex].staves)
+        return len(self.measures[0].slices[-1].parts[partIndex].staves)
+
+# Here's an attempt to be even better (there are some 2 piano scores that are wrong above)
+# but it made no difference.
+#         firstDataSlice: GridSlice | None = None
+#         for theSlice in self.measures[0].slices:
+#             if theSlice.isDataSlice:
+#                 firstDataSlice = theSlice
+#                 break
+#
+#         if firstDataSlice is None:
+#             return 0
+#
+#         return len(firstDataSlice.parts[partIndex].staves)
+#
+# Here's different attempt (matches a fix in humlib) that makes some scores better but
+# makes more scores worse, and one even crashes (because partIndex is out of range).
+#         return len(self.measures[0].slices[0].parts[partIndex].staves)
 
     '''
     //////////////////////////////
