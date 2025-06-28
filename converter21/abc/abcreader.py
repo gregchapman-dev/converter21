cac7d8d2e4b33eabbea4e85f7d7f197462778d0f
Greg Chapman <75333244+gregchapman-dev@users.noreply.github.com>
2025-06-28 14:32:28 -0700

    ABC import: initial implementation of AbcReader.

--- converter21/abc/abcreader.py
+++ converter21/abc/abcreader.py
@@ -8,12 +8,17 @@
 # License:       MIT, see LICENSE
 # ------------------------------------------------------------------------------
 import music21 as m21
+from converter21.abc.abc2xml import getXmlDocs
+from converter21.abc.abc2xml import fixDoctype
 
 class AbcReader:
     def __init__(self, dataString: str):
-        self.dataString: str = dataString
+        self.abcString: str = dataString
 
     def run(self) -> m21.stream.Score | m21.stream.Part | m21.stream.Opus:
-        # convert self.dataString (abc data) to musicxml data,
-        # then import _that_ into music21.
-        return m21.stream.Score()
+        # convert self.abcString (abc data) to musicxml data and then
+        # import _that_ into music21.
+        xmlDocs = getXmlDocs(self.abcString, num=1)
+        xmlStr: str = fixDoctype(xmlDocs[0])
+        return m21.converter.parseData(xmlStr, fmt='musicxml')
+
