a03569aa043cb0728450dfa48e20aa9ffd94d28b
Greg Chapman <75333244+gregchapman-dev@users.noreply.github.com>
2025-07-06 13:52:37 -0700

    Tests: first attempt at an abc2xml.py -> xml2abc.py diff test. It currently doesn't work for multi-tune ABC files (because musicdiff wants to compare scores, not opuses).

---
+++ tests/abc2xml2abc.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+# Usage: abc2xml2abc file.abc
+python converter21/abc/abc2xml.py -m 1 1000000 -o ./tmpxml $1
+python converter21/abc/xml2abc.py ./tmpxml/*.xml > $1.xml.abc
+python -m musicdiff -o t $1 $1.xml.abc
