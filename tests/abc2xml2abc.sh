8fd3538295e09fe70b89b163acd85218a802058b
Greg Chapman <75333244+gregchapman-dev@users.noreply.github.com>
2025-07-07 13:56:01 -0700

    Tests: fix abc2xml2abc.sh musicdiff command line.

--- tests/abc2xml2abc.sh
+++ tests/abc2xml2abc.sh
@@ -2,4 +2,4 @@
 # Usage: abc2xml2abc file.abc
 python converter21/abc/abc2xml.py -m 1 1000000 -o ./tmpxml $1
 python converter21/abc/xml2abc.py ./tmpxml/*.xml > $1.xml.abc
-python -m musicdiff -o t $1 $1.xml.abc
+python -m musicdiff -o t -- $1 $1.xml.abc
