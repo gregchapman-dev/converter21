a282d0b8aa9aa3fcd0a90c795b337b366be3d1a6
Greg Chapman <75333244+gregchapman-dev@users.noreply.github.com>
2025-07-07 20:04:02 -0700

    Tests: abc2xml2abc.sh finally working right.

--- tests/abc2xml2abc.sh
+++ tests/abc2xml2abc.sh
@@ -1,7 +1,8 @@
 #!/bin/zsh
 # Usage: abc2xml2abc file.abc
-python converter21/abc/abc2xml.py -m 1 1000000 -o ./tmpxml $1
+rm -r ./tmpxml
+python converter21/abc/abc2xml.py -m 0 1000000 -o ./tmpxml $1
 FILELIST=`ls ./tmpxml/*.xml`
-FILELIST=$(echo "$FILELIST" | tr '\n' ' ')
+FILELIST=($(echo "$FILELIST" | tr '\n' ' '))
 python converter21/abc/xml2abc.py $FILELIST > $1.xml.abc
 python -m musicdiff -o t -- $1 $1.xml.abc
