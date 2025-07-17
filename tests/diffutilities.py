fd488daf674d41fb88365a8470dfaf24823a1bc5
Greg Chapman <75333244+gregchapman-dev@users.noreply.github.com>
2025-07-16 17:42:32 -0700

    Tests: starting to restructure test file locations (and refactor the diffall*.py tests).

--- tests/diffutilities.py
+++ tests/diffutilities.py
@@ -4,12 +4,13 @@ import tempfile
 import sys
 import subprocess
 import json
+from io import TextIOWrapper
 
 from music21.base import VERSION_STR
 import music21 as m21
 
 from musicdiff import Visualization
-from musicdiff.annotation import AnnScore
+from musicdiff.annotation import AnnScore, AnnMetadataItem, AnnExtra
 from musicdiff import Comparison
 from musicdiff import DetailLevel
 
@@ -107,7 +108,7 @@ class DiffUtilities:
 
             # Use Stream.write instead of Opus.write (which will incorrectly
             # split into multiple files, one per score)
-            # success = score1.write(fp=writePath, fmt=outFmt, makeNotation=False)
+            # success = scoreOrOpus1.write(fp=writePath, fmt=outFmt, makeNotation=False)
             success = m21.stream.Stream.write(
                 scoreOrOpus1, fp=writePath, fmt=outFmt, makeNotation=False
             )
@@ -198,3 +199,429 @@ class DiffUtilities:
         numPad: int = len(longList) - len(shortList)
         for _ in range(0, numPad):
             shortList.append(m21.stream.Score())
+
+    @staticmethod
+    def runDiffAll(
+        listPath: Path,
+        inFmt: str,
+        outFmt: str,
+        outExt: str,
+        writeUsingVerovio: bool = False,
+        convertInputToMeiUsingVerovioBeforeReading: bool = False,
+    ):
+        goodPath: Path = Path(str(listPath.parent) + '/../results/' + str(listPath.stem)
+                                + '.goodList.txt')
+        badPath: Path = Path(str(listPath.parent) + '/../results/' + str(listPath.stem)
+                                + '.badList.txt')
+        resultsPath: Path = Path(str(listPath.parent) + '/../results/' + str(listPath.stem)
+                                + '.resultsList.txt')
+
+        fileList: list[str] = []
+        with open(listPath, encoding='utf-8') as listf:
+            s: str = listf.read()
+            fileList = s.split('\n')
+
+        with open(goodPath, 'w', encoding='utf-8') as goodf:
+            with open(badPath, 'w', encoding='utf-8') as badf:
+                with open(resultsPath, 'w', encoding='utf-8') as resultsf:
+                    for i, file in enumerate(fileList):
+                        if not file or file[0] == '#':
+                            # blank line, or commented out
+                            print(file)
+                            print(file, file=resultsf)
+                            resultsf.flush()
+                            continue
+
+                        if DiffUtilities.runDiffOne(
+                                Path(file),
+                                resultsf,
+                                inFmt,
+                                outFmt,
+                                outExt,
+                                writeUsingVerovio,
+                                convertInputToMeiUsingVerovioBeforeReading):
+                            resultsf.flush()
+                            print(file, file=goodf)
+                            goodf.flush()
+                        else:
+                            resultsf.flush()
+                            print(file, file=badf)
+                            badf.flush()
+                    resultsf.flush()
+                badf.flush()
+            goodf.flush()
+
+    @staticmethod
+    def runDiffOne(
+        inputPath: Path,
+        results: TextIOWrapper,
+        inFmt: str,
+        outFmt: str,
+        outExt: str,
+        writeUsingVerovio: bool = False,
+        convertInputToMeiUsingVerovioBeforeReading: bool = False,
+    ) -> bool:
+        # returns True if the test passed (no musicdiff differences found)
+        print(f'{inputPath}: ', end='')
+        print(f'{inputPath}: ', end='', file=results)
+        results.flush()
+
+        # import into music21
+        try:
+            scoreOrOpus1 = m21.converter.parse(inputPath, format='musicxml', forceSource=True)
+            if scoreOrOpus1 is None:
+                print('scoreOrOpus1 creation failure')
+                print('scoreOrOpus1 creation failure', file=results)
+                results.flush()
+                return False
+        except KeyboardInterrupt:
+            sys.exit(0)
+        except Exception as e:
+            print(f'scoreOrOpus1 creation crash: {e}')
+            print(f'scoreOrOpus1 creation crash: {e}', file=results)
+            results.flush()
+            return False
+
+        if not isinstance(scoreOrOpus1, (m21.stream.Score, m21.stream.Opus)):
+            print('scoreOrOpus1 is neither Score nor Opus')
+            print('scoreOrOpus1 is neither Score nor Opus', file=results)
+            results.flush()
+            return False
+
+        if not scoreOrOpus1.elements:
+            # empty score is valid result, but assume diff will be exact
+            # (export of empty score fails miserably)
+            print('numDiffs = 0 (empty scoreOrOpus1)')
+            print('numDiffs = 0 (empty scoreOrOpus1)', file=results)
+            results.flush()
+            return True
+
+        if not scoreOrOpus1.isWellFormedNotation():
+            print('scoreOrOpus1 not well formed')
+            print('scoreOrOpus1 not well formed', file=results)
+            results.flush()
+            return False
+
+        if inFmt in ('musicxml', 'mxl'):
+            # We know that MusicXML files can only contain one score.
+            if t.TYPE_CHECKING:
+                assert isinstance(scoreOrOpus1, m21.stream.Score)
+
+            # Some MusicXML files have abbreviations instead of chordKinds (e.g. 'min' instead of
+            # the correct 'minor').  Fix that before the diff is performed.
+            M21Utilities.fixupBadChordKinds(scoreOrOpus1, inPlace=True)
+
+            # Some MusicXML files have beams that go 'start'/'continue' when they should be
+            # 'start'/'stop'. fixupBadBeams notices that the next beam is a 'start', or is
+            # not present at all, and therefore patches that 'continue' to be a 'stop'.
+            M21Utilities.fixupBadBeams(scoreOrOpus1, inPlace=True)
+
+        # export score to outFmt (without any makeNotation fixups)
+
+        try:
+            success: bool = True
+            writePath = Path(tempfile.gettempdir())
+            writePath /= (inputPath.stem + '_Written')
+            writePath = writePath.with_suffix('.' + outExt)
+            # Use Stream.write instead of Opus.write (which will incorrectly
+            # split into multiple files, one per score)
+            # success = scoreOrOpus1.write(fp=writePath, fmt=outFmt, makeNotation=False)
+            success = m21.stream.Stream.write(
+                scoreOrOpus1, fp=writePath, fmt=outFmt, makeNotation=False
+            )
+            if not success:
+                print('export failed')
+                print('export failed', file=results)
+                results.flush()
+                return False
+        except KeyboardInterrupt:
+            sys.exit(0)
+        except Exception as e:
+            print(f'export crash: {e}')
+            print(f'export crash: {e}', file=results)
+            results.flush()
+            return False
+
+        # and then try to parse the exported file
+
+        try:
+            # compare the two music21 scores with musicdiff APIs:
+            scoreOrOpus2 = m21.converter.parse(writePath, format=outFmt, forceSource=True)
+            if scoreOrOpus2 is None:
+                print('scoreOrOpus2 creation failure')
+                print('scoreOrOpus2 creation failure', file=results)
+                results.flush()
+                return False
+        except KeyboardInterrupt:
+            sys.exit(0)
+        except Exception as e:
+            print(f'scoreOrOpus2 creation crash: {e}')
+            print(f'scoreOrOpus2 creation crash: {e}', file=results)
+            results.flush()
+            return False
+
+        if not isinstance(scoreOrOpus2, (m21.stream.Score, m21.stream.Opus)):
+            print('scoreOrOpus2 is neither Score nor Opus')
+            print('scoreOrOpus2 is neither Score nor Opus', file=results)
+            results.flush()
+            return False
+
+        if not scoreOrOpus2.elements:
+            print('scoreOrOpus2 was empty')
+            print('scoreOrOpus2 was empty', file=results)
+            results.flush()
+            return False  # empty scoreOrOpus2 is bad, because scoreOrOpus1 was not empty
+
+        if not scoreOrOpus2.isWellFormedNotation():
+            print('scoreOrOpus2 not well formed')
+            print('scoreOrOpus2 not well formed', file=results)
+            results.flush()
+            return False
+
+        # Some converters can read/write Score or Opus (full of scores).
+        # So we make lists 1 and 2 of scores, and loop over them, comparing.
+        score1List: list[m21.stream.Score] = DiffUtilities.getScoreList(scoreOrOpus1)
+        score2List: list[m21.stream.Score] = DiffUtilities.getScoreList(scoreOrOpus2)
+        DiffUtilities.padWithEmptyScores(score1List, score2List)
+
+        for i, (sc1, sc2) in enumerate(zip(score1List, score2List)):
+            # use musicdiff to compare the two music21 scores,
+            # and return whether or not they were identical
+            try:
+                annotatedScore1 = AnnScore(
+                    sc1,
+                    DetailLevel.AllObjects | DetailLevel.Style | DetailLevel.Metadata
+                )
+                annotatedScore2 = AnnScore(
+                    sc2,
+                    DetailLevel.AllObjects | DetailLevel.Style | DetailLevel.Metadata
+                )
+
+                op_list, cost = Comparison.annotated_scores_diff(
+                    annotatedScore1, annotatedScore2
+                )
+                numDiffs = len(op_list)
+                print(f'numDiffs = {numDiffs}')
+                print(f'numDiffs = {numDiffs}', file=results)
+                results.flush()
+                if numDiffs > 0:
+                    summ: str = '\t' + DiffUtilities.oplistSummary(op_list)
+                    print(summ)
+                    print(summ, file=results)
+
+                # print OMR-NED dict even if there are no diffs
+                omrnedOut: dict = Visualization.get_omr_ned_output(
+                    cost, annotatedScore1, annotatedScore2
+                )
+                jsonStr: str = json.dumps(omrnedOut)
+                print(jsonStr)
+                print(jsonStr, file=results)
+
+                textOut: str = Visualization.get_text_output(sc1, sc2, op_list)
+                if textOut:
+                    print(textOut)
+                    print(textOut, file=results)
+                    results.flush()
+
+                if numDiffs > 0:
+                    return False
+
+            except KeyboardInterrupt:
+                sys.exit(0)
+            except Exception as e:
+                print(f'musicdiff crashed: {e}')
+                print(f'musicdiff crashed: {e}', file=results)
+                results.flush()
+                return False
+
+        return True
+
+    @staticmethod
+    def oplistSummary(
+        op_list: list[tuple[str, t.Any, t.Any]],
+    ) -> str:
+        output: str = ''
+        counts: dict = {}
+
+        # print(f'op_list = {op_list}', file=sys.stderr)
+
+        counts['measure'] = 0
+        counts['voice'] = 0
+        counts['note'] = 0
+        counts['space'] = 0
+        counts['gracenote'] = 0
+        counts['beam'] = 0
+        counts['lyric'] = 0
+        counts['accidental'] = 0
+        counts['tuplet'] = 0
+        counts['tie'] = 0
+        counts['expression'] = 0
+        counts['articulation'] = 0
+        counts['notestyle'] = 0
+        counts['stemdirection'] = 0
+        counts['staffgroup'] = 0
+
+        for op in op_list:
+            # measure
+            if op[0] in ('insbar', 'delbar'):
+                counts['measure'] += 1
+            # voice
+            elif op[0] in ('voiceins', 'voicedel'):
+                counts['voice'] += 1
+            # note
+            elif op[0] in ('noteins',
+                            'notedel',
+                            'pitchnameedit',
+                            'inspitch',
+                            'delpitch',
+                            'headedit',
+                            'dotins',
+                            'dotdel'):
+                counts['note'] += 1
+            elif op[0] in ('editspace',
+                            'insspace',
+                            'delspace'):
+                counts['space'] += 1
+            elif op[0] in ('graceedit', 'graceslashedit'):
+                counts['gracenote'] += 1
+            elif op[0] in ('lyricins',
+                            'lyricdel',
+                            'lyricsub',
+                            'lyricedit',
+                            'lyricnumedit',
+                            'lyricidedit',
+                            'lyricoffsetedit',
+                            'lyricstyleedit'):
+                counts['lyric'] += 1
+            elif op[0] in ('editstyle',
+                           'editnoteshape',
+                           'editnoteheadfill',
+                           'editnoteheadparenthesis'):
+                counts['notestyle'] += 1
+            elif op[0] in ('editstemdirection'):
+                counts['stemdirection'] += 1
+            # beam
+            elif op[0] in ('insbeam',
+                            'delbeam',
+                            'editbeam'):
+                counts['beam'] += 1
+            # accidental
+            elif op[0] in ('accidentins',
+                            'accidentdel',
+                            'accidentedit'):
+                counts['accidental'] += 1
+            # tuplet
+            elif op[0] in ('instuplet',
+                            'deltuplet',
+                            'edittuplet'):
+                counts['tuplet'] += 1
+            # tie
+            elif op[0] in ('tieins',
+                            'tiedel'):
+                counts['tie'] += 1
+            # expression
+            elif op[0] in ('insexpression',
+                            'delexpression',
+                            'editexpression'):
+                counts['expression'] += 1
+            # articulation
+            elif op[0] in ('insarticulation',
+                            'delarticulation',
+                            'editarticulation'):
+                counts['articulation'] += 1
+            # staffgroup
+            elif op[0] in ('staffgrpins',
+                            'staffgrpdel',
+                            'staffgrpsub',
+                            'staffgrpnameedit',
+                            'staffgrpabbreviationedit',
+                            'staffgrpsymboledit',
+                            'staffgrpbartogetheredit',
+                            'staffgrppartindicesedit'):
+                counts['staffgroup'] += 1
+            # metadata
+            elif op[0] == 'mditemdel':
+                assert isinstance(op[1], AnnMetadataItem)
+                key = 'MD:' + op[1].key
+                if counts.get(key, None) is None:
+                    counts[key] = 0
+                counts[key] += 1
+            elif op[0] == 'mditemins':
+                assert isinstance(op[2], AnnMetadataItem)
+                key = 'MD:' + op[2].key
+                if counts.get(key, None) is None:
+                    counts[key] = 0
+                counts[key] += 1
+            elif op[0] == 'mditemsub':
+                assert isinstance(op[1], AnnMetadataItem)
+                assert isinstance(op[2], AnnMetadataItem)
+                if op[1].key != op[2].key:
+                    key = 'MD:' + op[1].key + '!=' + op[2].key
+                else:
+                    key = 'MD:' + op[1].key
+                if counts.get(key, None) is None:
+                    counts[key] = 0
+                counts[key] += 1
+            elif op[0] == 'mditemkeyedit':
+                assert isinstance(op[1], AnnMetadataItem)
+                assert isinstance(op[2], AnnMetadataItem)
+                key = 'MD:' + op[1].key + '!=' + op[2].key
+                if counts.get(key, None) is None:
+                    counts[key] = 0
+                counts[key] += 1
+            elif op[0] == 'mditemvalueedit':
+                assert isinstance(op[1], AnnMetadataItem)
+                assert isinstance(op[2], AnnMetadataItem)
+                key = 'MD:' + op[1].key
+                if counts.get(key, None) is None:
+                    counts[key] = 0
+                counts[key] += 1
+            elif op[0] == 'extradel':
+                # op[1] only
+                assert isinstance(op[1], AnnExtra)
+                key = op[1].kind
+                if counts.get(key, None) is None:
+                    counts[key] = 0
+                counts[key] += 1
+            elif op[0] == 'extrains':
+                # op[2] only
+                assert isinstance(op[2], AnnExtra)
+                key = op[2].kind
+                if counts.get(key, None) is None:
+                    counts[key] = 0
+                counts[key] += 1
+            elif op[0] in ('extrasub',
+                           'extracontentedit',
+                           'extrasymboledit',
+                           'extrainfoedit',
+                           'extraoffsetedit',
+                           'extradurationedit'):
+                # op[1] and op[2]
+                assert isinstance(op[1], AnnExtra)
+                assert isinstance(op[2], AnnExtra)
+                key = op[1].kind
+                if counts.get(key, None) is None:
+                    counts[key] = 0
+                counts[key] += 1
+            elif op[0] == 'extrastyleedit':
+                # op[1] and op[2]
+                assert isinstance(op[1], AnnExtra)
+                assert isinstance(op[2], AnnExtra)
+                key = op[1].kind + ':style'
+                if counts.get(key, None) is None:
+                    counts[key] = 0
+                counts[key] += 1
+
+
+        firstDone: bool = False
+        for k, v in counts.items():
+            if v == 0:
+                continue
+
+            if firstDone:
+                output += f', {k}:{v}'
+            else:
+                output += f'{k}:{v}'
+                firstDone = True
+
+        return output
