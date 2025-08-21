16b1fdd3078fbf22784e1b92d12acb2f7411c453
Greg Chapman <75333244+gregchapman-dev@users.noreply.github.com>
2025-08-20 17:26:53 -0700

    ABC import: first cut at producing metadata by hand.

--- converter21/abc/abc2xml.py
+++ converter21/abc/abc2xml.py
@@ -548,7 +548,7 @@ def splitHeaderVoices (abctext):
             mxm.metadata [ftype] = c + '\n' + field if c else field   # concatenate multiple info fields with new line as separator
             continue                    # skip W: lyrics
         if x2[:2] == '+:':              # field continuation
-            fln += x2[2:]
+            fln += ' ' + x2[2:]
             continue
         ro = r2.match (x2)              # single field on a line
         if ro:                          # field -> inline_field, escape all ']'
@@ -932,6 +932,7 @@ class MusicXml:
         s.tabStaff = ''     # == pid (part ID) for a tab staff
         s.edo = 12          # tuning
         s.mus53 = mOpt      # musescore playable edo53
+        s.header_fields_for_converter21 = {}
 
     def getAlter (s, acc):
         regacc = {'=':'0', '_':'-1', '__':'-2', '^':'1', '^^':'2'}
@@ -1963,6 +1964,8 @@ class MusicXml:
 
     def doHeaderField (s, fld, attrmap):
         type, value = fld.t[0], fld.t[1].replace ('%5d',']')    # restore closing brackets (see splitHeaderVoices)
+        c21val = s.header_fields_for_converter21.get(type, '')
+        s.header_fields_for_converter21[type] = c21val + '\n' + value if c21val else value
         if not value:    # skip empty field
             return
         if type == 'M':
--- converter21/abc/abcreader.py
+++ converter21/abc/abcreader.py
@@ -9,14 +9,14 @@
 # ------------------------------------------------------------------------------
 import typing as t
 import re
-from xml.etree.ElementTree import Element
+import copy
+# from xml.etree.ElementTree import Element
 
 import music21 as m21
 
 from converter21.shared import M21Utilities
-from converter21.abc.abc2xml import getXmlDocs
-from converter21.abc.abc2xml import fixDoctype
-from converter21.abc.abc2xml import expand_abc_include
+from converter21.shared import SharedConstants
+from converter21.abc import abc2xml
 
 class ABCImportException(Exception):
     pass
@@ -27,6 +27,7 @@ class AbcReader:
         self.abcTuneByNumber: dict[str, str] = {}
         self.numberForAbcTune: dict[str, str] = {}
         self.abcTunesInDocumentOrder: list[str] = []
+        self.headerFieldsByTuneNumber: dict[str, dict[str, str]] = {}
 
     def run(
         self,
@@ -35,12 +36,11 @@ class AbcReader:
         # convert abc data to musicxml data and then import
         # _that_ into music21.
         xmlStrs: list[str] = []
-        abcNumbers: list[str] = []
         numStr: str
         if not self.abcTuneByNumber:
-            # a bit of code stolen from getXmlDocs that finds the X:n number for
+            # a bit of code stolen from abc2xml.getXmlDocs that finds the X:n number for
             # each tune (if there's more than one tune), and the associated tune.
-            abctext: str = expand_abc_include(self.abcString)
+            abctext: str = abc2xml.expand_abc_include(self.abcString)
             fragments: list[str] = re.split(r'^\s*X:', abctext, flags=re.M)
             preamble: str = fragments[0]
             tunes: list[str] = fragments[1:]
@@ -57,18 +57,18 @@ class AbcReader:
                 self.abcTuneByNumber[numStr] = fullTuneText
                 self.numberForAbcTune[fullTuneText] = numStr
 
+        abcNumbers: list[str] = []
         if number is None:
             # all the tunes in the ABC data (in doc order)
-            xmlDocs: list[Element] = []
             abcNumbers = []
             for tune in self.abcTunesInDocumentOrder:
                 numStr = self.numberForAbcTune[tune]
-                xmlDocs.extend(getXmlDocs(self.abcTuneByNumber[numStr]))
                 abcNumbers.append(numStr)
-
-            for xmlDoc in xmlDocs:
-                self.tweakXmlDoc(xmlDoc)
-                xmlStr: str = fixDoctype(xmlDoc)
+                xmlDoc = abc2xml.getXmlDocs(self.abcTuneByNumber[numStr])[0]
+                self.headerFieldsByTuneNumber[numStr] = copy.copy(
+                    abc2xml.mxm.header_fields_for_converter21
+                )
+                xmlStr: str = abc2xml.fixDoctype(xmlDoc)
                 xmlStrs.append(xmlStr)
         else:
             numStr = str(number)
@@ -76,17 +76,19 @@ class AbcReader:
                 raise ABCImportException(
                     f'cannot find requested reference number in source file: {number}'
                 )
-            xmlDoc = getXmlDocs(self.abcTuneByNumber[numStr])[0]
-            self.tweakXmlDoc(xmlDoc)
-            xmlStrs = [fixDoctype(xmlDoc)]
             abcNumbers = [numStr]
+            xmlDoc = abc2xml.getXmlDocs(self.abcTuneByNumber[numStr])[0]
+            self.headerFieldsByTuneNumber[numStr] = copy.copy(
+                abc2xml.mxm.header_fields_for_converter21
+            )
+            xmlStrs = [abc2xml.fixDoctype(xmlDoc)]
 
         if len(xmlStrs) == 1:
             # return a Score
             score = m21.converter.parseData(xmlStrs[0], fmt='musicxml')
             if t.TYPE_CHECKING:
                 assert isinstance(score, m21.stream.Score)
-            score.metadata.number = abcNumbers[0]
+            score.metadata = self.computeAbcMetadata(self.headerFieldsByTuneNumber[abcNumbers[0]])
             M21Utilities.fixupBadBeams(score, inPlace=True)
             return score
 
@@ -95,34 +97,121 @@ class AbcReader:
         opus = m21.stream.Opus()
         for xmlStr, numStr in zip(xmlStrs, abcNumbers):
             score = m21.converter.parseData(xmlStr)
-            score.metadata.number = numStr
+            if t.TYPE_CHECKING:
+                assert isinstance(score, m21.stream.Score)
+            score.metadata = self.computeAbcMetadata(self.headerFieldsByTuneNumber[numStr])
             opus.coreAppend(score)
         opus.coreElementsChanged()
         M21Utilities.fixupBadBeams(opus, inPlace=True)
+        # if self.preambleHeaderFields:
+        #     self.computeAbcMetadata(opus, self.preambleHeaderFields)
         return opus
 
     @staticmethod
-    def tweakXmlDoc(xmlDoc: Element):
-        # munge any miscellaneous metadata names (e.g. 'notes') into music21-style
-        # namespaced names (e.g. 'dcterms:description')
-        miscfields = xmlDoc.findall('*/*/miscellaneous-field')
-        for mf in miscfields:
-            if 'name' in mf.attrib:
-                name: str = mf.attrib['name']
-                if name == 'notes':
-                    mf.attrib['name'] = 'dcterms:description'
-                    continue
-                if name == 'history':
-                    mf.attrib['name'] = 'dcterms:description'
-                    continue
-                if name in ('origin', 'area'):
-                    if mf.text and (';' in mf.text or ',' in mf.text):
-                        # locale (city, town, or village) of composition
-                        mf.attrib['name'] = 'humdrum:OPC'
+    def computeAbcMetadata(headerFields: dict[str, str]) -> m21.metadata.Metadata:
+        def addValue(md: m21.metadata.Metadata, mdKey: str, hfValue: str):
+            md.add(mdKey, hfValue)
+
+        def addCustomValue(md: m21.metadata.Metadata, mdKey: str, hfValue: str):
+            md.addCustom(mdKey, hfValue)
+
+        def addValues(md: m21.metadata.Metadata, mdKey: str, hfValue: str):
+            hfValues: list[str] = splitValues(hfValue)
+            md.add(mdKey, hfValues)
+
+        def addCustomValues(md: m21.metadata.Metadata, mdKey: str, hfValue: str):
+            hfValues: list[str] = splitValues(hfValue)
+            md.addCustom(mdKey, hfValues)
+
+        def splitValues(hfValue: str) -> list[str]:
+            return hfValue.split('\n')
+
+        md = m21.metadata.Metadata()
+
+        # music21 is already in the md.software list, add converter21, and then
+        # we will add any I:abc-creator we happen to see as well.
+        verStr: str = SharedConstants._CONVERTER21_NAME_AND_VERSION
+        md.add('software', verStr)
+
+        for hfKey, hfValue in headerFields.items():
+            if hfKey in ('K', 'L', 'M', 'Q', 'P', 'U'):
+                # header data that is not metadata
+                continue
+
+            if hfKey in ('N', 'H', 'W', 'R', 'G'):
+                # There is no standard metadata key in music21 for these, so we
+                # make up a custom namespace:name such as 'abc:N', etc.
+                # N = notes: such as references to other tunes which are similar,
+                #   details on how the original notation of the tune was converted
+                #   to abc, etc
+                # H = history: designed for multi-line notes, stories and anecdotes
+                # W = untimed lyrics: to be printed after the music, for example
+                # R = rhythm: an indication of the type of tune (e.g. hornpipe, double jig,
+                #   single jig, 48-bar polka, etc).
+                # G: grouping key (used for so many different things)
+                addCustomValues(md, 'abc:' + hfKey, hfValue)
+            elif hfKey == 'X':
+                addValue(md, 'number', hfValue)
+            elif hfKey == 'T':
+                addValues(md, 'title', hfValue)
+            elif hfKey == 'C':
+                addValues(md, 'composer', hfValue)
+            elif hfKey == 'B':  # 'book'
+                addValues(md, 'parentTitle', hfValue)
+            elif hfKey == 'F':  # file URL
+                addValues(md, 'filePath', hfValue)
+            elif hfKey == 'D':  # discography
+                # instead of abc:D, we use Humdrum's existing recording title item
+                # Note that MEI import from verovio-translated ABC -> MEI will need
+                # to read abc:D and set it as humdrum:RTL in the music21 metadata.
+                addCustomValues(md, 'humdrum:RTL', hfValue)  # 'album title'
+            elif hfKey in ('O', 'A'):
+                # 'A' is deprecated, we will read it, but write it as 'O'
+                vals = splitValues(hfValue)
+                for val in vals:
+                    if ';' in val or ',' in val:
+                        addValue(md, 'localeOfComposition', val)
                     else:
-                        # country of composition
-                        mf.attrib['name'] = 'humdrum:OCY'
-                    continue
-                if name == 'book':
-                    # parentTitle
-                    mf.attrib['name'] = 'humdrum:OPR'
+                        addValue(md, 'countryOfComposition', val)
+            elif hfKey in ('I', 'Z'):
+                # These are prefixed with 'abc-something '
+                vals = splitValues(hfValue)
+                for val in vals:
+                    abcNameAndValue = val.split(' ', 1)
+                    if len(abcNameAndValue) == 1:
+                        if hfKey == 'Z':
+                            # no space-delimited abcName, so val is the encoder
+                            addValue(md, 'electronicEncoder', val)
+                            continue
+
+                    abcName = abcNameAndValue[0]
+                    if not abcName.startswith('abc'):
+                        if hfKey == 'Z':
+                            # no parseable abcName, so val is the encoder
+                            addValue(md, 'electronicEncoder', val)
+                            continue
+                        if hfKey == 'I':
+                            # we ignore unparseable I: abcNames because there are a lot,
+                            # and they are generally not metadata.
+                            continue
+
+                    abcValue = abcNameAndValue[1]
+                    if hfKey == 'Z':
+                        if abcName == 'abc-transcription':
+                            addValue(md, 'electronicEncoder', abcValue)
+                        elif abcName == 'abc-edited-by':
+                            addValue(md, 'electronicEditor', abcValue)
+                        elif abcName == 'abc-copyright':
+                            addValue(md, 'copyright', abcValue)
+                        else:
+                            addCustomValue(md, 'abc:Z:' + abcName, abcValue)
+                    elif hfKey == 'I':
+                        # ignore everything but 'abc-creator'
+                        if abcName == 'abc-creator':
+                            addValue(md, 'software', abcValue)
+                        else:
+                            addCustomValue(md, 'abc:I:' + abcName, abcValue)
+            else:
+                pass  # print(f'need to support {hfKey}')
+
+        return md
