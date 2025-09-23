d55d4c2886db3e99b21e929bd5888ba3c39305ab
Greg Chapman <75333244+gregchapman-dev@users.noreply.github.com>
2025-09-23 11:59:08 -0700

    Tests: add showdiff MEI -> ABC.

---
+++ tests/showdiffMeiImportVsABCExport.py
@@ -0,0 +1,23 @@
+import argparse
+from pathlib import Path
+
+from tests.diffutilities import DiffUtilities
+
+# ------------------------------------------------------------------------------
+
+'''
+    main entry point (parse arguments and run test)
+'''
+
+parser = argparse.ArgumentParser()
+parser.add_argument('input_file')
+parser.add_argument('-n', '--num', default=None)
+args = parser.parse_args()
+
+DiffUtilities.runShowDiff(
+    inputPath=Path(args.input_file),
+    inFmt='mei',
+    outFmt='abc',
+    outExt='abc',
+    scoreNum=int(args.num) if args.num is not None else None
+)
