cd19b59d5d29923993ce0de47404fe46082de564
Greg Chapman <75333244+gregchapman-dev@users.noreply.github.com>
2025-10-09 15:10:11 -0700

    MEI export: Pull that "fix up bad durations" change, since it causes interesting issues. I'll investigate later.

--- converter21/mei/meiwriter.py
+++ converter21/mei/meiwriter.py
@@ -96,7 +96,8 @@ class MeiWriter:
 
         # Third: deal with various duration problems (we see this e.g. after import of a
         # Photoscore-generated MusicXML file)
-        M21Utilities.fixupBadDurations(self._m21ScoreOrOpus, inPlace=True)
+        # Caused interesting issues, so don't do this
+        # M21Utilities.fixupBadDurations(self._m21ScoreOrOpus, inPlace=True)
 
         # Check that all parts (in all scores) have the same number of measures.
         err: str = M21Utilities.reportUnwritableScore(
