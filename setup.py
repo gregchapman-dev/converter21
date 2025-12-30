582be6bb4e5be5e491971c0fd0739b36e6eadfb1
Greg Chapman <75333244+gregchapman-dev@users.noreply.github.com>
2025-12-30 12:51:42 -0800

    Update dependencies and version number.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@
 import setuptools
 
 # must be kept up to date with converter21/shared/sharedconstants.py:_CONVERTER21_VERSION et al
-converter21version = '3.5.0'
+converter21version = '4.0.0'
 
 if __name__ == '__main__':
     setuptools.setup(
@@ -61,7 +61,7 @@ if __name__ == '__main__':
         python_requires='>=3.10',
 
         install_requires=[
-            'music21>=9.7',
+            'music21>=9.9.1',
         ],
 
         project_urls={
