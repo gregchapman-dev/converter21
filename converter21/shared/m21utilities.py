cb9e25393f321a88ac02da518461eb0875971af5
Greg Chapman <75333244+gregchapman-dev@users.noreply.github.com>
2026-01-11 14:28:21 -0800

    Oops, left an exception raise in place for malformed HSL color.

--- converter21/shared/m21utilities.py
+++ converter21/shared/m21utilities.py
@@ -5105,7 +5105,7 @@ class M21Utilities:
                 return
 
         except Exception:
-            raise  # for testing.  pass later.
+            pass  # for testing.  pass later.
 
         return
 
